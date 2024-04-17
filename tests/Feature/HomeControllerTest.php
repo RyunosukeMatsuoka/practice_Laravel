8ae539252435635180518ca0fb5a0f4936bf856b
Ryunosuke Matsuokaa <sirosakana014kenko@outlook.jp>
2024-04-18 00:32:34 +0900

    ルーティング修正

--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -16,43 +16,56 @@ class HomeControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_authenticated_user_can_access_home_page()
+    public function test_authenticated_user_can_see_articles_page()
     {
         $user = User::factory()->create();
         $this->actingAs($user);
 
-        // サンプルデータを入れてテストをしたい
-        /* $articles = Article::factory()->count(5)->create();
-        $article_tags = Article_tag::factory()->count(5)->create();
-        $tags = Tag::factory()->count(5)->create();
-        $users = User::factory()->count(5)->create(); */
+        $articles = Article::factory(10)->create();
+        $tags = Tag::factory(5)->create();
+        // $article_tags = Article_tag::factory(5)->create();
+        // $users = User::factory(3)->create();
 
         $response = $this->get(route('articles'));
 
         $response->assertStatus(200);
         $response->assertViewIs('conduit.home');
 
-        $response->assertViewHas('articles');
-        $response->assertViewHas('article_tags');
-        $response->assertViewHas('tags');
-        $response->assertViewHas('users');
-        /* $response->assertViewHas('articles', $articles);
-        $response->assertViewHas('article_tags', $article_tags);
-        $response->assertViewHas('tags', $tags);
-        $response->assertViewHas('users', $users); */
+        foreach ($articles as $article) {
+            $response->assertSee($article->title);
+            $response->assertSee($article->outline);
+        }
+
+        foreach ($tags as $tag) {
+            $response->assertSee($tag->name);
+        }
     }
 
-    public function test_unauthenticated_user_can_access_home_page()
+    public function test_unauthenticated_user_can_see_articles_page()
     {
+        // $users = User::factory(3)->create();
+        $this->actingAs(User::factory()->create(), 'web');
+        $articles = Article::factory(10)->create();
+        $tags = Tag::factory(5)->create();
+        // $article_tags = Article_tag::factory(5)->create();
+
         $response = $this->get(route('articles'));
 
         $response->assertStatus(200);
         $response->assertViewIs('conduit.Unauth_home');
 
-        $response->assertViewHas('articles');
-        $response->assertViewHas('article_tags');
-        $response->assertViewHas('tags');
-        $response->assertViewHas('users');
+        foreach ($articles as $article) {
+            $response->assertSee($article->title);
+            $response->assertSee($article->outline);
+        }
+
+        foreach ($tags as $tag) {
+            $response->assertSee($tag->name);
+        }
+
+        /* foreach ($users as $user) {
+            $response->assertSee($user->name);
+        } */
     }
 
     public function test_authenticated_user_can_access_tag_page()
@@ -107,5 +120,44 @@ class HomeControllerTest extends TestCase
         $response->assertViewHas('tag', $tag);
         $response->assertViewHas('tags', Tag::all());
         $response->assertViewHas('users', $users);
+    }
+
+    public function test_guest_user_is_redirected_to_loginPage()
+    {
+        $response = $this->get('/myArticles/1');
+
+        $response->assertRedirect('/signIn');
+    }
+
+    public function test_user_can_login_and_is_redirected_to_ownArticles()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $response = $this->post('/signIn/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect('/myArticles/'.$user->id);
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_with_invalid_credentials_cannot_login()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $response = $this->post('/signIn/login', [
+            'email' => 'test@example.com',
+            'password' => 'wrong_password',
+        ]);
+
+        $response->assertSessionHasErrors('login_error');
+        $this->assertGuest();
     }
 }
